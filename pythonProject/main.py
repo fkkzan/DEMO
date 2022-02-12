ec215ec94648b5f0af3eccecc4d20c4baf2ed26a
Zan Zan <aaa>
2022-02-12 12:35:38 +0800

    This is our project initial state

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,2 +1,8 @@
-import _json
-print ("hi")
+class CF2104Q:
+ def calavgheight(totalheight, totalstudents):
+    avgheight = totalheight/totalstudents
+
+totalheight = float(0)
+totalstudents = int(0)
+
+file = open(heights.txt)
